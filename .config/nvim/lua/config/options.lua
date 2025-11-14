d14e19cad8fc0ba2be430a863c593f571a9dd800
Dmitrii Chizha <me@filaco.dev>
2025-11-14 13:17:32 +0300

    feat(nvim): add cangjie treesitter

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if not vim.filetype.match { filename = 'cangjie' } then
+  vim.filetype.add {
+    extension = {
+      cj = 'cangjie',
+    },
+  }
+end
+
+vim.treesitter.language.register('cangjie', { 'cj' })
