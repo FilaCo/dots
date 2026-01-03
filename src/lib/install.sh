851b2d06b9c7aca9c9b00c3eb7bf71331a25fad3
FilaCo <me@filaco.dev>
2026-01-04 00:28:47 +0300

    feat(hypr): update hypr configs

--- src/lib/install.sh
+++
@@ -1,7 +0,0 @@
-install_yay() {
-  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-}
-
-install_rust() {
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-}
--- src/lib/load_extra_zsh_conf.sh
+++
@@ -1,3 +0,0 @@
-for conf in "$XDG_CONFIG_HOME"/zshrc.d/*.sh; do
-  source "$conf"
-done
