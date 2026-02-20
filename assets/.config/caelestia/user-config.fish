00d61e7bd698040091ceef38401f71de033787b2
Dmitrii Chizha <chizha.dmitrii@huawei.com>
2026-02-20 12:10:17 +0300

    feat: workdesktop specific configs

--- assets/.config/caelestia/user-config.fish
+++ assets/.config/caelestia/user-config.fish
@@ -2,3 +2,5 @@
 if uwsm check may-start 1>/dev/null
     uwsm start hyprland-uwsm.desktop
 end
+
+# source ~/.cangjie/envsetup.fish 2>/dev/null
---
+++ assets/.config/fish/completions/cjdev.fish
@@ -0,0 +1,1 @@
+complete --command cjdev --no-files --arguments "(env _CJDEV_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) cjdev)" --condition "env _CJDEV_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) cjdev"
